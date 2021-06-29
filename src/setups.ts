cbbc008bd7ce9e01bf73287d420f189d33408288
KodyJKing <kody.j.king@gmail.com>
2021-06-28 21:22:20 -0700

    tweak

--- src/setups.ts
+++ src/setups.ts
@@ -2,6 +2,7 @@ import Body from "./Body"
 import { Vector } from "./Vector"
 
 const colorPalette = [ "#264653", "#2A9D8F", "#E9C46A", "#F4A261", "#E76F51" ]
+const staticCircleColor = "#d1ccb6"
 
 export function setup1( canvas: HTMLCanvasElement, bodies: Body[] ) {
     for ( let i = 0; i < 1500; i++ ) {
@@ -21,7 +22,7 @@ export function setup1( canvas: HTMLCanvasElement, bodies: Body[] ) {
     bodies.push( new Body( {
         pos: new Vector( canvas.width / 2, canvas.height / 2 ),
         radius: 100,
-        color: "#d1ccb6",
+        color: staticCircleColor,
         isStatic: true
     } ) )
 }
@@ -45,31 +46,31 @@ export function setup2( canvas: HTMLCanvasElement, bodies: Body[] ) {
     bodies.push( new Body( {
         pos: new Vector( canvas.width / 2, canvas.height / 4 ),
         radius: 100,
-        color: "#d1ccb6",
+        color: staticCircleColor,
         isStatic: true
     } ) )
     bodies.push( new Body( {
         pos: new Vector( canvas.width / 2 + 200, canvas.height / 2 ),
         radius: 100,
-        color: "#d1ccb6",
+        color: staticCircleColor,
         isStatic: true
     } ) )
     bodies.push( new Body( {
         pos: new Vector( canvas.width / 2 - 200, canvas.height / 2 ),
         radius: 100,
-        color: "#d1ccb6",
+        color: staticCircleColor,
         isStatic: true
     } ) )
     bodies.push( new Body( {
         pos: new Vector( canvas.width, canvas.height ),
         radius: 100,
-        color: "#d1ccb6",
+        color: staticCircleColor,
         isStatic: true
     } ) )
     bodies.push( new Body( {
         pos: new Vector( 0, canvas.height ),
         radius: 100,
-        color: "#d1ccb6",
+        color: staticCircleColor,
         isStatic: true
     } ) )
 }
